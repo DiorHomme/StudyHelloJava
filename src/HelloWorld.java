f062ee9757a9b1c67afaa764d55eb2f320340454
song <song@218.38.137.28>
2022-09-09 22:54:40 +0900

    제발 공부좀 잘되자부탁한

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("늘 고생만 시켜서 미안해");
 		System.out.println("언제나 사랑해");
-		System.out.println("소원아 사랑합니다^^");
+		System.out.println("소원아 사랑합니다");
 	}
 }
